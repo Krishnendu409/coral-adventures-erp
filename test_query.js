08eac0b1042c445ff63571a6557cfdfce24c295f
Krishnendu409 <krishnendu.mitmpl2025@learner.manipal.edu>
2026-07-05 19:41:25 +0530

    chore: remove development and testing scripts

--- test_query.js
+++
@@ -1,8 +0,0 @@
-const Database = require('better-sqlite3');
-const db = new Database('./data/coral_adventures.db');
-try {
-  const params = db.prepare("SELECT * FROM business_parameters ORDER BY category, parameter").all();
-  console.log("Success! Found", params.length, "parameters.");
-} catch(err) {
-  console.error("Error:", err);
-}
