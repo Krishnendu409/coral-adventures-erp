293aa706d5105d2753180df9dd71abab53c2da17
Krishnendu <Krishnendu409@users.noreply.github.com>
2026-07-10 00:11:14 +0530

    Restructure project: Move app to root, remove portable node & wrappers

--- push_to_github.bat
+++
@@ -1,6 +0,0 @@
-"C:\Program Files\Git\cmd\git.exe" init
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Completely fixed project"
-"C:\Program Files\Git\cmd\git.exe" branch -M master
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/Krishnendu409/coral-adventures-erp.git
-"C:\Program Files\Git\cmd\git.exe" push -u origin master
